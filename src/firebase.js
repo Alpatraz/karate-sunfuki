f684189da58f83563bb1b6dbd012c988f8ea7edb
Guillaume SAINT-ETIENNE <guillaumesaint-etienne@MacBook-Air-de-Guillaume.local>
2025-06-13 09:30:52 -0400

    Initial commit sans clé exposée

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7HIu-9pMFZWdWWQV6g9a7BhP8vo8Ufw8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "karate-sunfuki-boutique.firebaseapp.com",
   projectId: "karate-sunfuki-boutique",
-  storageBucket: "karate-sunfuki-boutique.firebaseapp.app",
+  storageBucket: "karate-sunfuki-boutique.appspot.com",
   messagingSenderId: "323637671299",
   appId: "1:323637671299:web:6c1fcf75f1cedcc633b2b6",
   measurementId: "G-KHGKGMKD1M"
